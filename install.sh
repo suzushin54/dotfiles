1c26f9997fe7b6563f0f9cd2a6ddfedf4044e91a
suzushin54 <s.suzuki416@gmail.com>
2020-05-22 14:38:31 +0900

    Add a ideavimrc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ln -s ~/rep/dotfiles/.vimrc ~/.vimrc
+ln -s ~/rep/dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/rep/dotfiles/.zshrc ~/.zshrc
 ln -s ~/rep/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/rep/dotfiles/.kubectl_aliases ~/.kubectl_aliases
