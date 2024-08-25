941b94276faa07a9bca009bbf16317a7af17aae9
suzushin54 <mkm.snicr@gmail.com>
2024-08-25 22:02:57 +0900

    Initial commit: Add dotfiles managed by chezmoi

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,100 @@
+set -x LANG ja_JP.UTF-8
+
+fish_add_path /usr/local/bin
+fish_add_path /usr/local/sbin
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
+fish_add_path $HOME/.local/share/mise/shims
+fish_add_path /usr/local/bin/idea
+fish_add_path /opt/homebrew/opt/php@8.2/bin
+fish_add_path /opt/homebrew/opt/php@8.2/sbin
+fish_add_path $HOME/.composer/vendor/bin
+fish_add_path $HOME/pear/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/go/bin
+fish_add_path /usr/local/go/bin
+fish_add_path /usr/local/bin/protoc
+fish_add_path $HOME/.local/share/mise/installs/terraform/1.8.4/bin
+fish_add_path $HOME/development/flutter/bin
+
+# Homebrew
+eval (/opt/homebrew/bin/brew shellenv)
+
+# mise
+mise activate fish | source
+
+# Editor
+set -x EDITOR /Applications/MacVim.app/Contents/MacOS/Vim
+
+# Golang
+set -x GOPATH $HOME/go
+
+# direnv
+direnv hook fish | source
+
+# basic aliase
+alias md "mkdir"
+alias cls "clear"
+alias .. "cd .."
+alias ... "cd ../.."
+alias .... "cd ../../.."
+alias ..... "cd ../../../.."
+alias ...... "cd ../../../../.."
+alias ls "eza --color-scale --git --git-ignore --time-style=iso -a -T -F -h -l -L=1"
+alias ll "eza --color-scale --git --git-ignore --time-style=iso -a -T -F -h -l -L=1"
+alias rm "rm -i"
+alias cp "cp -i"
+alias mv "mv -i"
+alias vi "vim"
+alias cat "cat -n"
+alias less "less -NM"
+
+# git aliases
+alias pl "git pull"
+alias push "git push"
+alias commit "git commit"
+alias diff "git diff"
+alias gco "git checkout"
+alias ga "git add"
+alias gl "git log"
+alias gpl "git log --pretty='format:%C(yellow)%h %C(green)%cd %C(reset)%s %C(red)%d %C(cyan)[%an]' --date=iso"
+alias gb "git branch"
+alias gf "git fetch"
+alias gs "git switch"
+alias gr "git restore"
+alias gcp "git cherry-pick"
+
+# docker aliases
+alias dps "docker ps"
+alias compose "docker compose"
+alias cps "docker compose ps"
+alias cu "docker compose up"
+alias cud "docker compose up -d"
+alias cdn "docker compose down"
+
+# ghq func
+function ghqcd
+    set -l query $argv
+    set -l repo (ghq list | fzf --query=$query)
+    if test -n "$repo"
+        cd (ghq root)/$repo
+    end
+end
+
+# fish history
+function fish_user_key_bindings
+    bind \cr 'peco_select_history (commandline -b)'
+end
+
+# exec eza after cd
+function cd
+    builtin cd $argv
+    eza --color-scale --git --git-ignore --time-style=iso -a -T -F -h -l -L=1
+end
+
+# Peco key binding
+function fish_user_key_bindings
+    bind \cr peco_select_history
+end
+
+
---
+++ dot_wezterm.lua
@@ -0,0 +1,58 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Color scheme
+config.color_scheme = 'Kanagawa'
+
+-- Font configuration
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 15.0
+
+-- Window configuration
+config.window_background_opacity = 0.75
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 2,
+  bottom = 2,
+}
+
+-- Tab bar
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Key bindings
+config.keys = {
+  {
+    key = 't',
+    mods = 'CMD',
+    action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentTab{confirm=true},
+  },
+}
+
+-- Hotkey configuration
+config.leader = { key = 'Space', mods = 'OPT', timeout_milliseconds = 1000 }
+config.keys = {
+  {
+    key = 'Space',
+    mods = 'LEADER|OPT',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
+-- Set initial position and size (adjust as needed)
+config.initial_rows = 40
+config.initial_cols = 120
+
+return config
+
