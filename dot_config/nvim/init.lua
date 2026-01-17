1cb9a4ff3e70f15d01f7736c259d24df383dc621
suzushin54 <mkm.snicr@gmail.com>
2026-01-17 21:57:09 +0900

    feat: lazyvim, lazygit settings

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,31 +1,41 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", lazypath })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Basic options
+-- Setup leader key before lazy
 vim.g.mapleader = " "
-vim.opt.termguicolors = true
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.g.maplocalleader = "\\"
 
--- Load plugins
-require("lazy").setup(require("plugins"))
+-- Disable deprecation warnings (mason repo rename)
+vim.g.lazyvim_check_deprecations = false
 
--- Keymaps (IDE ライク検索)
-local map = vim.keymap.set
-map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
-map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>",  { desc = "Grep" })
-map("n", "<leader>fb", "<cmd>Telescope buffers<cr>",    { desc = "Buffers" })
-map("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", { desc="Symbols" })
-
--- ファイラー（oil）
-map("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory (oil)" })
-
--- ファイルツリー(nvim-tree)
-map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file tree" })
-map("n", "<leader>ef", "<cmd>NvimTreeFindFile<cr>", { desc = "Find file in tree" })
-map("n", "<leader>et", "<cmd>NvimTreeFocus<cr>", { desc = "Focus on file tree" })
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- Language extras (must come before custom plugins)
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- Custom plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = false,
+    version = false,
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = { enabled = true },
+})
